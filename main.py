eb14af5804267722183babc9e674d01aceee1fb3
Shakti Sarada Prasad <83201466+shakti-sarada@users.noreply.github.com>
2024-01-30 03:29:54 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from KidneyDiseaseClassifier import logger
+from KidneyDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to Loggings.") 
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> {STAGE_NAME} completed <<<<<<\n\nx=========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
--- template.py
+++ template.py
@@ -18,7 +18,7 @@ list_of_files = [
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
-    "dvc.yaml"
+    "dvc.yaml",
     "params.yaml",
     "requirements.txt",
     "setup.py",
