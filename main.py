c67b86ed74268905928fcf6d03e934a864db5e08
Shakti Sarada Prasad <83201466+shakti-sarada@users.noreply.github.com>
2024-02-01 06:10:22 +0530

    model training added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from KidneyDiseaseClassifier import logger
 from KidneyDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from KidneyDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from KidneyDiseaseClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+
+
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -20,8 +23,20 @@ STAGE_NAME = "Prepare base model"
 try:
     logger.info(f"***********")
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
-    obj = PrepareBaseModelTrainingPipeline()
-    obj.main()
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>> {STAGE_NAME} completed <<<<<<\n\nx=========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"**************")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>>> {STAGE_NAME} completed <<<<<<\n\nx=========x")
 
 except Exception as e:
