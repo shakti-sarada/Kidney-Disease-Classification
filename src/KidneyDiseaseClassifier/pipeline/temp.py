9f7737cf6dcbc8a98a9de45bd4298baed5420153
Shakti Sarada Prasad <83201466+shakti-sarada@users.noreply.github.com>
2024-02-12 01:59:08 +0530

    temp removed

--- src/KidneyDiseaseClassifier/pipeline/temp.py
+++
@@ -1,7 +0,0 @@
-# from KidneyDiseaseClassifier.pipeline.prediction import PredictionPipeline
-
-# obj = PredictionPipeline()
-
-# x = obj.predict("artifacts/data_ingestion/KidneyDisease Dataset/Normal/Normal- (1).jpg")
-# print("From Temp File ",x)
-
