4cf89c12bf7cc8b2071008976dd7f72d702db3bb
Shakti Sarada Prasad <83201466+shakti-sarada@users.noreply.github.com>
2024-02-12 01:57:11 +0530

    requirements and setup added

---
+++ src/KidneyDiseaseClassifier/pipeline/temp.py
@@ -0,0 +1,7 @@
+# from KidneyDiseaseClassifier.pipeline.prediction import PredictionPipeline
+
+# obj = PredictionPipeline()
+
+# x = obj.predict("artifacts/data_ingestion/KidneyDisease Dataset/Normal/Normal- (1).jpg")
+# print("From Temp File ",x)
+
